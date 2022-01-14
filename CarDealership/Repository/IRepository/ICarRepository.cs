800314a58e33c1b6655f30c643d60e9fadac1709
Arthur Ryoo <jene0523@gmail.com>
2022-01-13 22:38:17 -0800

    Add car interface

---
+++ CarDealership/Repository/IRepository/ICarRepository.cs
@@ -0,0 +1,13 @@
+﻿using CarDealership.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealership.Repository.IRepository
+{
+    public interface ICarRepository
+    {
+        Task<Car> GetCar(int id);
+    }
+}
